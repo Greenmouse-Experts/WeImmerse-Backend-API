b44f13b3ad5599aeaedcd4015057d4aaee676ec9
manuelExtra <Olaleyeemmanuel23@gmail.com>
2025-02-24 15:01:42 +0100

    feature: add ping server

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import { Server } from 'socket.io';
 import createExpressApp from './services/express.service';
 import sequelizeService from './services/sequelize.service'; // Adjusted to match your service structure
 import apiRouter from './routes/authRoute'; // Import your routes here
+import { Request, Response } from 'express';
 // import { configureSocket } from "./services/socket.service";
 
 dotenv.config();
@@ -19,6 +20,10 @@ const io = new Server(server, {
   cors: {
     origin: '*', // Change to specific origins in production
   },
+});
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello world.');
 });
 
 app.use('/api', apiRouter); // Mount the router to /api
