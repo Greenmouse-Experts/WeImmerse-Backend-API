2781c55d3f5904f9924e218b35e3630241780c84
manuelExtra <Olaleyeemmanuel23@gmail.com>
2025-03-05 13:01:31 +0100

    fix: implement db name change

---
+++ dist/config/database.js
@@ -0,0 +1,17 @@
+"use strict";
+require('dotenv/config');
+module.exports = {
+    dialect: process.env.DB_DIALECT,
+    host: process.env.DB_HOST,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+    define: {
+        timestamps: true,
+    },
+    migrationStorage: 'sequelize',
+    migrationStorageTableName: 'SequelizeMeta',
+    migrations: ['database/migrations'],
+};
+//# sourceMappingURL=database.js.map
